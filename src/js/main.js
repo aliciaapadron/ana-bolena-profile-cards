9c0f10fbe3f872eae2b6d65e49895038f3cdbf04
patriciapallares <patriciapallaresgonzalez.com>
2022-03-23 14:22:02 +0100

    added toggle event in fill-in

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+const fillTitle = document.querySelector(".js-fill-title");
+const fillSection = document.querySelector('.js-fill-container');
+
+fillTitle.addEventListener('click', () => {
+  console.log('funciona?');
+  fillSection.classList.toggle('collapsed');
+});
