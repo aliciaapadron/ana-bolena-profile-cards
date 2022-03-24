2a174844f5205ef8dcd1fb52fd8e71c8644cb1eb
patriciapallares <patriciapallaresgonzalez.com>
2022-03-24 14:01:31 +0100

    added js toggle to arrow

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,18 @@
 
 console.log(">> Ready :)");
 
-const fillTitle = document.querySelector(".js-fill-title");
+const fillTitle = document.querySelector('.js-fill-title');
 const fillSection = document.querySelector('.js-fill-container');
 
 fillTitle.addEventListener('click', () => {
   console.log('funciona?');
   fillSection.classList.toggle('collapsed');
 });
+
+const arrow = document.querySelector('.js-arrow')
+
+arrow.addEventListener('click', () => {
+  console.log('funciona:D');
+  arrow.classList.toggle('rotate');
+});
+
