1ec97c2f82e687f28ec06ed507d587a25fccceeb
Rosa Codina <rosa.codina90@gmail.com>
2022-03-24 20:53:17 +0100

    function and event done in Design drop down menu. I could not apply the rotate animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,25 @@
 'use strict';
 
-console.log(">> Ready :)");
+//DESIGN DROP-DOWN
+//Constants
+const designBox = document.querySelector('.js-designBox');
+const colourDropDown = document.querySelector('.js-colour');
+
+//Functions
+designBox.addEventListener('click', () => {
+  console.log('mierda');
+  colourDropDown.classList.toggle('collapsed');
+});
+
+const designArrow = document.querySelector('.js-designArrow');
+
+designArrow.addEventListener('click', () => {
+  console.log('vale');
+  designArrow.classList.toogle('rotate');
+});
+
+//FILL IN DROP-DOWN
+console.log('>> Ready :)');
 
 const fillTitle = document.querySelector('.js-fill-title');
 const fillSection = document.querySelector('.js-fill-container');
@@ -10,10 +29,9 @@ fillTitle.addEventListener('click', () => {
   fillSection.classList.toggle('collapsed');
 });
 
-const arrow = document.querySelector('.js-arrow')
+const arrow = document.querySelector('.js-arrow');
 
 arrow.addEventListener('click', () => {
   console.log('funciona:D');
   arrow.classList.toggle('rotate');
 });
-
