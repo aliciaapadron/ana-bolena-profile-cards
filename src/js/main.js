6a265a97b75ef9d54c37470f141848de37b60e82
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-25 12:41:15 +0100

    querySelectorAll with arrows

--- src/js/main.js
+++ src/js/main.js
@@ -13,8 +13,24 @@ formTitle.addEventListener('click', () => {
   formSection.classList.toggle('collapsed');
 });
 
-const arrow = document.querySelector('.js-arrow');
+// const arrow = document.querySelector('.js-arrow');
+// arrow.addEventListener('click', () => {
+//   arrow.classList.toggle('rotate');
+// });
 
-arrow.addEventListener('click', () => {
-  arrow.classList.toggle('rotate');
-});
+// const formTitle2 = document.querySelectorAll('.js-form-title');
+// let formSection2 = document.querySelectorAll('.js-form-container');
+
+// for (const container of formTitle2) {
+//   container.addEventListener('click', () => {
+//     const sectionContainer = formSection2;
+//     sectionContainer.classList.toggle('collapsed');
+//   });
+// }
+
+const arrow2 = document.querySelectorAll('.js-arrow');
+for (const arrows of arrow2) {
+  arrows.addEventListener('click', () => {
+    arrows.classList.toggle('rotate');
+  });
+}
