bc85fe66bb456f38a973a9e4a7d45fbc3786330a
Alicia Padrón <aliciaapadron@gmail.com>
2022-03-25 12:16:09 +0100

    add js in form

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,19 @@
 
 //DESIGN DROP-DOWN
 //Constants
-const designBox = document.querySelector('.js-designBox');
-const colourDropDown = document.querySelector('.js-colour');
-const designArrow = document.querySelector('.js-designArrow');
-
-//Functions
-designBox.addEventListener('click', () => {
-  console.log('mierda');
-  colourDropDown.classList.toggle('collapsed');
-  console.log('vale');
-  designArrow.classList.toggle('rotate');
-});
 //FILL IN DROP-DOWN
 
 console.log('>> Ready :)');
 
-const fillTitle = document.querySelector('.js-fill-title');
-const fillSection = document.querySelector('.js-fill-container');
+const formTitle = document.querySelector('.js-form-title');
+const formSection = document.querySelector('.js-form-container');
 
-fillTitle.addEventListener('click', () => {
-  console.log('funciona?');
-  fillSection.classList.toggle('collapsed');
+formTitle.addEventListener('click', () => {
+  formSection.classList.toggle('collapsed');
 });
 
 const arrow = document.querySelector('.js-arrow');
 
 arrow.addEventListener('click', () => {
-  console.log('funciona:D');
   arrow.classList.toggle('rotate');
 });
